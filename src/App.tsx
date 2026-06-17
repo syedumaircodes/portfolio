6e6cbf4d750525c097fe9742b5bcbd944be62fac
Syed Umair Ali <syedumairali.617@gmail.com>
2026-06-17 13:00:58 +0500

    added reduced motion options

--- src/App.tsx
+++ src/App.tsx
@@ -6,23 +6,25 @@ import BlogPost from "./pages/BlogPost"
 import Resources from "./pages/Resources"
 import NotFound from "./pages/NotFound"
 import { HelmetProvider } from "react-helmet-async"
-
+import { MotionConfig } from "motion/react"
 function App() {
   return (
     <HelmetProvider>
-      <Router>
-        <div className="relative min-h-screen bg-background text-foreground selection:bg-primary/30">
-          <main className="flex-1">
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/blogs" element={<Blogs />} />
-              <Route path="/blogs/:slug" element={<BlogPost />} />
-              <Route path="/resources" element={<Resources />} />
-              <Route path="*" element={<NotFound />} />
-            </Routes>
-          </main>
-        </div>
-      </Router>
+      <MotionConfig reducedMotion="user">
+        <Router>
+          <div className="relative min-h-screen bg-background text-foreground selection:bg-primary/30">
+            <main className="flex-1">
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/blogs" element={<Blogs />} />
+                <Route path="/blogs/:slug" element={<BlogPost />} />
+                <Route path="/resources" element={<Resources />} />
+                <Route path="*" element={<NotFound />} />
+              </Routes>
+            </main>
+          </div>
+        </Router>
+      </MotionConfig>
     </HelmetProvider>
   )
 }
