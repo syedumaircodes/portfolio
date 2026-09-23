babad4aa918a1f3c3bdc846deef57ef70ed47e78
Syed Umair Ali <syedumairal.617@gmail.com>
2026-09-23 22:10:18 +0500

    removed other pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,7 @@
-// src/App.tsx
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import Home from "./pages/Home"
-import Blogs from "./pages/Blogs"
-import BlogPost from "./pages/BlogPost"
-import Resources from "./pages/Resources"
 import NotFound from "./pages/NotFound"
 import { HelmetProvider } from "react-helmet-async"
-// Import MotionConfig, LazyMotion, and domAnimation
 import { MotionConfig, LazyMotion, domAnimation } from "motion/react"
 
 function App() {
@@ -19,9 +14,6 @@ function App() {
               <main className="flex-1">
                 <Routes>
                   <Route path="/" element={<Home />} />
-                  <Route path="/blogs" element={<Blogs />} />
-                  <Route path="/blogs/:slug" element={<BlogPost />} />
-                  <Route path="/resources" element={<Resources />} />
                   <Route path="*" element={<NotFound />} />
                 </Routes>
               </main>
