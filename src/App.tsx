1cbd45552ca420d5fa245433c82538c5bbf01138
Syed Umair Ali <syedumairal.617@gmail.com>
2026-09-25 11:37:57 +0500

    removed unused packages

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,20 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import Home from "./pages/Home"
 import NotFound from "./pages/NotFound"
 import { HelmetProvider } from "react-helmet-async"
-import { MotionConfig, LazyMotion, domAnimation } from "motion/react"
 
 function App() {
   return (
     <HelmetProvider>
-      <LazyMotion features={domAnimation}>
-        <MotionConfig reducedMotion="user">
-          <Router>
-            <div className="relative min-h-screen bg-background text-foreground selection:bg-primary/30">
-              <main className="flex-1">
-                <Routes>
-                  <Route path="/" element={<Home />} />
-                  <Route path="*" element={<NotFound />} />
-                </Routes>
-              </main>
-            </div>
-          </Router>
-        </MotionConfig>
-      </LazyMotion>
+      <Router>
+        <div className="relative min-h-screen bg-background text-foreground selection:bg-primary/30">
+          <main className="flex-1">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </main>
+        </div>
+      </Router>
     </HelmetProvider>
   )
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,12 @@
 import path from "path"
 import tailwindcss from "@tailwindcss/vite"
 import react from "@vitejs/plugin-react"
-import mdx from "@mdx-js/rollup"
 
 import { defineConfig } from "vite"
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    {
-      enforce: "pre",
-      ...mdx({
-        jsxImportSource: "react", // Crucial for React 19 / Vite 7
-      }),
-    },
-    react(),
-    tailwindcss(),
-  ],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
